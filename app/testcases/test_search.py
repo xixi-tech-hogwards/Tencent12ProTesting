eb51b95e50d3102e33734c8f7f248764ad92bbb2
AndyJee <andy_jee@sina.com>
2020-05-22 16:53:02 +0800

    Appium 第一节课安装与录制 课后作业

--- app/testcases/test_search.py
+++ app/testcases/test_search.py
@@ -1,7 +1,7 @@
 # This sample code uses the Appium python client
 # pip install Appium-Python-Client
 # Then you can paste this into a file and simply run with Python
-
+import pytest
 from appium import webdriver
 from appium.webdriver.common.mobileby import MobileBy
 from selenium.webdriver.common.by import By
@@ -11,38 +11,56 @@ from selenium.webdriver.common.by import By
 改造2： 改造成可维护的代码形态，绝对不允许有绝对路径的存在 
 改造3： 将自动生成的find_element_by_**  改造成find_element(MobileBy.)
 改造4： 添加断言 
+改造5： 合理使用 setup_class, setup 方法 
 """
 
 
 class TestXueQiu:
-    def setup(self):
-        print("setup")
+    def setup_class(self):
+        print("setup_class")
         caps = {}
         caps["platformName"] = "Android"
         caps["deviceName"] = "127.0.0.1:7555"
         caps["appPackage"] = "com.xueqiu.android"
         caps["appActivity"] = ".view.WelcomeActivityAlias"
         caps['noReset'] = "true"
+        caps['skipServerInstallation'] = True
+        caps['skipDeviceInitialization'] = True
 
         self.driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
         self.driver.implicitly_wait(10)
 
-    def teardown(self):
-        print("teardown")
+    def teardown_class(self):
+        print("teardown_class")
         self.driver.quit()
 
-    def test_search(self):
-        print("search")
+    def setup(self):
+        print("setup")
 
+    def teardown(self):
+        print("teardown")
+        self.driver.find_element(MobileBy.ID, 'com.xueqiu.android:id/action_close').click()
+
+    @pytest.mark.parametrize('searchkey,searchresult', [
+        ("alibaba", "阿里巴巴"),
+        ("jd", "京东")
+    ])
+    def test_search(self, searchkey, searchresult):
+        print("search")
         el1 = self.driver.find_element(MobileBy.ID, "com.xueqiu.android:id/tv_search")
         el1.click()
 
         el2 = self.driver.find_element(MobileBy.ID, "com.xueqiu.android:id/search_input_text")
-        el2.send_keys("alibaba")
+        el2.send_keys(searchkey)
 
-        el3 = self.driver.find_element(MobileBy.XPATH, "//*[@text='阿里巴巴']")
+        el3 = self.driver.find_element(MobileBy.XPATH, f"//*[@text='{searchresult}']")
         el3.click()
 
-        el4 = self.driver.find_element(MobileBy.XPATH,
-                                       "//*[@text='加自选']")
-        el4.click()
+        el4 = self.driver.find_elements(MobileBy.XPATH,
+                                        f"//*[@text='{searchresult}']/../..//*[@text='加自选']")
+        if len(el4) > 0:
+            el4[0].click()
+            self.driver.find_element(MobileBy.XPATH,
+                                     f"//*[@text='{searchresult}']/../..//*[@text='已添加']")
+        else:
+            print("已加自选")
